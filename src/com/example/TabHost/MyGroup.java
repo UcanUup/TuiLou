cff59e1e232cc190df9a9624cb88969c97fc3b20
UcanUup <lyy112526@qq.com>
2014-09-16 15:31:45 +0800

    增加展开菜单

--- src/com/example/TabHost/MyGroup.java
+++ src/com/example/TabHost/MyGroup.java
@@ -8,26 +8,27 @@ import android.view.View.OnLongClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ListView;
+import android.widget.ExpandableListView;
 
 import com.example.R;
 import com.example.adapter.GroupAdapter;
 
 public class MyGroup extends Fragment {
-	private ListView groupListView;
+	private ExpandableListView groupListView;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		View rootView = inflater.inflate(R.layout.my_group, container,
+		View rootView = inflater.inflate(R.layout.group_expandable_list, container,
 				false);
 		
-		groupListView = (ListView)rootView.findViewById(R.id.groupListView);
+		groupListView = (ExpandableListView)rootView.findViewById(R.id.groupListView);
 		
 		//设置设配器
-		GroupAdapter groupAdapter = new GroupAdapter(getActivity());
-		groupListView.setAdapter(groupAdapter);
+		groupListView.setGroupIndicator(null);
+		groupListView.setAdapter(new GroupAdapter(getActivity()));
+		groupListView.expandGroup(0);
 		
 		//点击每一项时的触发事件
 		groupListView.setOnItemClickListener(new OnItemClickListener() {
--- src/com/example/adapter/GroupAdapter.java
+++ src/com/example/adapter/GroupAdapter.java
@@ -1,54 +1,131 @@
 package com.example.adapter;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.R;
 
-public class GroupAdapter extends BaseAdapter {
-	private String[] item1 = {"小组1", "小组2", "小组3", "小组4", "小组5"};
-	private String[] item2 = {"高数", "模电", "C++", "计算机网络", "编译原理"};
+public class GroupAdapter extends BaseExpandableListAdapter {
+	private List<String> groups = new ArrayList<String>();
+	private List<List<String>> childs = new ArrayList<List<String>>();
+
 	private Context mContext;
-		
+	
 	public GroupAdapter(Context mContext) {
-		// TODO Auto-generated constructor stub
 		super();
+		
+		//每个小组名
+		groups.add("我的参与");
+		groups.add("我的创建");
+		
+		//第一组
+		List<String> child1 = new ArrayList<String>();
+		child1.add("高数");
+		child1.add("C++");
+		child1.add("模电");
+		childs.add(child1);
+		
+		//第二组
+		List<String> child2 = new ArrayList<String>();
+		child2.add("计算机网络");
+		child2.add("编译原理");
+		childs.add(child2);
+		
 		this.mContext = mContext;
 	}
 
 	@Override
-	public int getCount() {
+	public int getGroupCount() {
 		// TODO Auto-generated method stub
-		return item1.length;
+		return groups.size();
 	}
 
 	@Override
-	public Object getItem(int position) {
+	public int getChildrenCount(int groupPosition) {
 		// TODO Auto-generated method stub
-		return item1[position];
+		return childs.get(groupPosition).size();
 	}
 
 	@Override
-	public long getItemId(int position) {
+	public Object getGroup(int groupPosition) {
 		// TODO Auto-generated method stub
-		return position;
+		return groups.get(groupPosition);
 	}
 
 	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		// 设置每一个项显示的内容
-		RelativeLayout layout = (RelativeLayout)LayoutInflater.from(mContext).inflate(R.layout.message_item, null);
-		TextView tv1 = (TextView)layout.findViewById(R.id.textView1);		
-		tv1.setText(item1[position]);
+	public Object getChild(int groupPosition, int childPosition) {
+		// TODO Auto-generated method stub
+		return childs.get(groupPosition).get(childPosition);
+	}
+
+	@Override
+	public long getGroupId(int groupPosition) {
+		// TODO Auto-generated method stub
+		return groupPosition;
+	}
+
+	@Override
+	public long getChildId(int groupPosition, int childPosition) {
+		// TODO Auto-generated method stub
+		return childPosition;
+	}
+
+	@Override
+	public boolean hasStableIds() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public View getGroupView(int groupPosition, boolean isExpanded,
+			View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		RelativeLayout layout = (RelativeLayout)LayoutInflater.from(mContext).inflate(R.layout.group, null);
+		ImageView ie = (ImageView)layout.findViewById(R.id.isExpand);
+		TextView tv = (TextView)layout.findViewById(R.id.TextView1);
 		
-		TextView tv2 = (TextView)layout.findViewById(R.id.textView2);		
-		tv2.setText(item2[position]);
+		//设置组内容
+		tv.setText(groups.get(groupPosition));
 
+		if (isExpanded) {
+			ie.setImageResource(R.drawable.expand);
+		}
+		else {
+			ie.setImageResource(R.drawable.no_expand);
+		}
+		
 		return layout;
 	}
+
+	@Override
+	public View getChildView(int groupPosition, int childPosition,
+			boolean isLastChild, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		RelativeLayout layout = (RelativeLayout)LayoutInflater.from(mContext).inflate(R.layout.child, null);
+		TextView tv = (TextView)layout.findViewById(R.id.TextView1);
+		
+		//设置子内容
+		tv.setText(childs.get(groupPosition).get(childPosition));
+	
+		return layout;
+	}
+
+	@Override
+	public boolean isChildSelectable(int groupPosition, int childPosition) {
+		// TODO Auto-generated method stub
+		
+		//返回true时点击会有闪烁
+		return true;
+	}
+	
 }
