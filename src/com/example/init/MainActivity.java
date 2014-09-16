8adfe76da6d10ba217a3a4ebde11298bab5b6b51
UcanUup <lyy112526@qq.com>
2014-09-16 15:53:28 +0800

    最终版本II

--- src/com/example/init/MainActivity.java
+++ src/com/example/init/MainActivity.java
@@ -1,22 +1,18 @@
 package com.example.init;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import com.example.R;
 import com.example.lib.UserInfo;
 import com.example.sqlite.UserDatabase;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+public class MainActivity extends Activity {
 
-public class MainActivity extends ActionBarActivity {
-	
 	// 设置启动画面延迟的时间
 	private final static int DISPLAY_LENGTH = 1300; 
 	
@@ -24,16 +20,34 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		
+		// 一个Handler用于处理延迟事件，控制打开应用画面的时间
+		new Handler().postDelayed(new Runnable() {  
+            public void run() {  
+            	// 清除数据库内容
+				UserDatabase userDatabase = new UserDatabase(MainActivity.this);
+				userDatabase.read();
+				
+				// 之前已经登陆过，则跳转到主页
+				if (UserInfo.email != null && UserInfo.userName != null) {
+					Intent intent = new Intent();
+	    			intent.setClass(MainActivity.this, HomeActivity.class);
+	    			startActivity(intent);
+				}
+				// 之前未登录过，则跳转到登陆页面
+				else {
+					Intent intent = new Intent();
+					intent.setClass(MainActivity.this, LoginActivity.class);
+					startActivity(intent);
+				}
+                finish();  
+            }  
+  
+        }, DISPLAY_LENGTH);  
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
@@ -50,47 +64,4 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			
-			// 一个Handler用于处理延迟事件，控制打开应用画面的时间
-			new Handler().postDelayed(new Runnable() {  
-	            public void run() {  
-	            	// 清除数据库内容
-					UserDatabase userDatabase = new UserDatabase(getActivity());
-					userDatabase.read();
-					
-					// 之前已经登陆过，则跳转到主页
-					if (UserInfo.email != null && UserInfo.userName != null) {
-						Intent intent = new Intent();
-		    			intent.setClass(getActivity(), HomeActivity.class);
-		    			startActivity(intent);
-					}
-					// 之前未登录过，则跳转到登陆页面
-					else {
-						Intent intent = new Intent();
-						intent.setClass(getActivity(), LoginActivity.class);
-						startActivity(intent);
-					}
-	                getActivity().finish();  
-	            }  
-	  
-	        }, DISPLAY_LENGTH);  
-			
-			return rootView;
-		}
-	}
-
 }
